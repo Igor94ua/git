d037ea7443979d01142ab6944bb593700e451c79
Igor <sukachov94@gmail.com>
2020-01-29 22:14:05 +0200

    second commit

--- docs/js/main.js
+++ docs/js/main.js
@@ -9,19 +9,6 @@
    });
  });
 
- $(document).ready(function () {
-   $(".button").click(function () {
-     var value = $(this).attr("data-filter");
-     var elem = $(".elem");
-     if (value == "all") {
-       $(elem).show("500");
-     } else {
-       $(elem).not("." + value).hide("500");
-       $(elem).filter("." + value).show("500");
-     }
-   });
- })
-
  document.querySelector('.menu-icon-wrapper').onclick = function () {
    document.querySelector('.menu-icon').classList.toggle('menu-icon-active');
    document.querySelector('.mobile_nav').classList.toggle('mobile_nav-active');
@@ -33,3 +20,77 @@
   document.querySelector('.mobile_nav').classList.toggle('mobile_nav-active');
   document.querySelector('.overlay_menu').classList.toggle('overlay_menu-active');  
  }
+
+ $(document).ready(function () {
+  $('#page-nav').onePageNav({
+    currentClass: 'active',
+    changeHash: false,
+    scrollSpeed: 750,
+    scrollThreshold: 0.5,
+    filter: '',
+    easing: 'swing',
+    begin: function () {},
+    end: function () {},
+    scrollChange: function ($currentListItem) {}
+  });
+})
+
+$(document).ready(function () {
+  $('#portfolio-filter').mixItUp();
+});
+
+    //FORM VALIDATE
+$(document).ready(function () {  
+    $('#contact-form').validate({
+      rules: {
+          email: {
+              required: true,
+              email: true
+          },
+          theme: {
+              required: true
+          },
+          message: {
+              required: true
+          }
+      },
+      messages: {
+          email: {
+              required: 'Введите Ваш email',
+              email: 'Отсутсвует символ @'
+          },
+          theme: {
+              required: 'Введите тему сообщения'
+          },
+          message: {
+              required: 'Введите текст сообщения'
+          }
+      },
+      submitHandler: function (form) {
+          ajaxFormSubmit();
+      }
+  });
+});
+
+  // Функция AJAX запрса на сервер
+$(document).ready(function () {
+  function ajaxFormSubmit() {
+      let string = $("#contact-form").serialize(); // Соханяем данные введенные в форму в строку. 
+
+      //Формируем ajax запрос
+      $.ajax({
+          type: "POST", // Тип запроса - POST
+          url: "php/mail.php", // Куда отправляем запрос
+          data: string, // Какие даные отправляем, в данном случае отправляем переменную string
+
+          // Функция если все прошло успешно
+          success: function (html) {
+              $("#contact-form").slideUp(800);
+              $('#answer').html(html);
+          }
+      });
+
+      // Чтобы по Submit больше ничего не выполнялось - делаем возврат false чтобы прервать цепочку срабатывания остальных функций
+      return false;
+  }
+});
