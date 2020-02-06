f5b77ebb5f2ed90effc59bf285bfc013ef01ed28
Igor <sukachov94@gmail.com>
2020-02-06 18:43:54 +0200

    Six commit

---
+++ docs/page-myWorks/job-forest/main.js
@@ -0,0 +1,12 @@
+
+  document.querySelector('.menu-icon-wrapper').onclick = function () {
+    document.querySelector('.menu-icon').classList.toggle('menu-icon-active');
+    document.querySelector('.mobile_nav').classList.toggle('mobile_nav-active');
+    document.querySelector('.overlay_menu').classList.toggle('overlay_menu-active');  
+  }
+ 
+  document.querySelector('.overlay_menu').onclick = function(){
+   document.querySelector('.menu-icon').classList.toggle('menu-icon-active');
+   document.querySelector('.mobile_nav').classList.toggle('mobile_nav-active');
+   document.querySelector('.overlay_menu').classList.toggle('overlay_menu-active');  
+  }
